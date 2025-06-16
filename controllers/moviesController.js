9cc7dcb11c949c02d285e5dd2e507a753e616d67
dannyarena <arenadanny71@gmail.com>
2025-06-16 20:36:57 +0200

    add movies controller with getAllMovies and getMovieById functions

---
+++ controllers/moviesController.js
@@ -0,0 +1,57 @@
+const db = require ('../data/db');
+
+function getAllMovies(req, res) {
+    const sql = 'SELECT * FROM movies';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Errore nella query' });
+        }
+        res.json(results);
+    });
+}
+
+function getMovieById(req, res) {
+    const movieId = req.params.id;
+    const sql = `
+    SELECT m.*, r.id AS review_id, r.name, r.vote, r.text
+    FROM movies m
+    LEFT JOIN reviews r ON m.id = r.movie_id
+    WHERE m.id = ?
+    `;
+    db.query(sql, [movieId], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Errore nella query' });
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'Film non trovato'});
+        }
+        const film = {
+            id: results[0].id,
+            title: results[0].title,
+            director: results[0].director,
+            genre: results[0].genre,
+            release_year: results[0].release_year,
+            abstract: results[0].abstract,
+            image: results[0].image,
+            created_at: results[0].created_at,
+            updated_at: results[0].updated_at,
+            reviews: []  
+        }
+        results.forEach(row => {
+            if (row.review_id) {
+                film.reviews.push({
+                    name: row.name,
+                    vote: row.vote,
+                    text: row.text
+                });
+            }
+        })
+        res.json(film);
+    });
+}
+
+module.exports = {
+    getAllMovies,
+    getMovieById
+};
