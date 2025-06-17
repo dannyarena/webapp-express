86445772e79d05139269e026686c786960757a1c
dannyarena <arenadanny71@gmail.com>
2025-06-17 15:39:54 +0200

    add middleware for not found route

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const moviesRouter = require('./routers/movies')
 app.use(express.json());
 app.use('/movies', moviesRouter)
 
-
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'Rotta non trovata'
+    });
+});
 
 
 
