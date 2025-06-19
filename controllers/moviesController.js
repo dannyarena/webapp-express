42fa01f6a8af250acc2edbd403fd0f907f63cd5a
dannyarena <arenadanny71@gmail.com>
2025-06-19 18:05:46 +0200

    add review functionality to movies router

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -60,22 +60,24 @@ function addReview(req, res) {
     VALUES (?, ?, ?, ?)
     `
 
-    db.query(sql, [name, vote, text, movie_id], (err, result) => {
+    db.query(sql, [name, vote, text, movieId], (err, result) => {
+        
+        if (err) {
+            console.error("Errore nell'inserimento;", err);
+            return res.status(500).json({ error: "Errore nel salvataggio"});
+        }
 
-    });
-
-    if (err) {
-        console.error("Errore nell'inserimento;", err);
-        return res.status(500).json({ error: "Errore nel salvataggio"});
-    }
-
-    res.status(201).json({
+        res.status(201).json({
         message: "Recensione salvata con successo",
         insertId: result.insertId
+        
+    });
+    
     });
 }
 
 module.exports = {
     getAllMovies,
-    getMovieById
+    getMovieById,
+    addReview
 };
--- routers/movies.js
+++ routers/movies.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovies, getMovieById } = require('../controllers/moviesController');
+const { getAllMovies, getMovieById, addReview } = require('../controllers/moviesController');
 
 router.get('/', getAllMovies);
 router.get('/:id', getMovieById);
+router.post('/:id/reviews', addReview);
 
 module.exports = router;
